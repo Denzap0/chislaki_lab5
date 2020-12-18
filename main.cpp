165bc035f601d495b28bc8d25b9167840f60761d
denzap <denzap@bk.ru>
2020-12-19 00:16:36 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  lab5
+//
+//  Created by MacBook on 12/19/20.
+//
+
+#include <iostream>
+#include <cmath>
+#define eps 1e-4
+using namespace std;
+
+double f1(double x)
+{
+    return pow(x,0.5)/(1 + x * x);
+}
+
+
+double trapezoid(double a, double b, int n){
+    double h = (b - a)/n;
+    double sum = f1(a) + f1(b);
+    for(int i = 1; i <= n -1; i++){
+        sum += 2 * f1(a + i * h);
+    }
+    sum *= h/2;
+    return  sum;
+}
+
+double sympson(double a, double b, int n){
+    double h = (b - a)/n;
+    double sum = f1(a) + f1(b);
+    for(int i = 1; i <= n -1; i++){
+        if(i % 2 == 0){
+            sum += 2 * f1(a + i * h);
+        }else{
+            sum += 4 * f1(a + i * h);
+        }
+    }
+    sum *= h/3;
+    return  sum;
+}
+
+
+void rungeCriterior(){
+    double sum = 0, sumPrev = 0;
+    int n1= 1, n2 = 1;
+    do{
+        sumPrev = sum;
+        sum = trapezoid(0, 1.0, n1);
+        n1++;
+    }while(abs(sum - sum) > 3 * eps);
+    cout << sum << endl;
+    sum = 0;
+    sumPrev = 0;
+    do{
+        sumPrev = sum;
+        sum = sympson(0, 1.0, n2);
+        n2++;
+    }while(abs(sum - sumPrev) > 15 * eps);
+    cout << sum;
+}
+
+int main(){
+    rungeCriterior();
+}
