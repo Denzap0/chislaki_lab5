9ff7492e1b5a2ec49b45cbabbf11c76a80f5befc
denzap <denzap@bk.ru>
2020-12-30 13:11:22 +0300

    3

--- main.cpp
+++ main.cpp
@@ -12,54 +12,53 @@ using namespace std;
 
 double f1(double x)
 {
-    return pow(x,0.5)/(1 + x * x);
+    return pow(x, 0.5) * sin(x * x);
 }
 
 
-double trapezoid(double a, double b, int n){
-    double h = (b - a)/n;
+double trapezoid(double a, double b, double h){
     double sum = f1(a) + f1(b);
-    for(int i = 1; i <= n -1; i++){
+    for(int i = 1; i <= (b - a)/h - 1; i++){
         sum += 2 * f1(a + i * h);
     }
     sum *= h/2;
     return  sum;
 }
 
-double sympson(double a, double b, int n){
-    double h = (b - a)/n;
+double sympson(double a, double b, double h){
     double sum = f1(a) + f1(b);
-    for(int i = 1; i <= n -1; i++){
-        if(i % 2 == 0){
-            sum += 2 * f1(a + i * h);
-        }else{
-            sum += 4 * f1(a + i * h);
-        }
+    for (int i = 1; i <= (b - a) / h; i += 2){
+        sum += 4 * f1(a + h * i);
+    }
+    for (int i = 2; i < (b - a) / h - 1; i += 2) {
+        sum += 2 * f1(a + h * i);
     }
     sum *= h/3;
     return  sum;
 }
 
 
-void rungeCriterior(){
-    double sum = 0, sumPrev = 0;
-    int n = 1;
+void rungeCriterior(double a, double b){
+    double sumPrev = 0, sum = 0;
+    double h = b - a;
     do{
         sumPrev = sum;
-        sum = trapezoid(0, 1.0, n);
-        n++;
-    }while(abs(sum - sum) > 3 * eps);
+        sum = trapezoid(a, b, h);
+        h /= 2;
+    }while(abs(sum - sumPrev) > 3 * eps);
     cout << sum << endl;
-    sum = 0;
     sumPrev = 0;
+    sum = 0;
+    h = (b - a)/2;
     do{
         sumPrev = sum;
-        sum = sympson(0, 1.0, n);
-        n++;
+        sum = sympson(a, b, h);
+        h/= 2;
     }while(abs(sum - sumPrev) > 15 * eps);
     cout << sum;
+    
 }
 
 int main(){
-    rungeCriterior();
+    rungeCriterior(0, 1);
 }
